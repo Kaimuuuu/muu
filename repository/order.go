3ad7c9a6822dc718285677ca58420053e4e677af
melon <vitaya.p@ku.th>
2024-01-21 00:54:20 +0700

    Sort by createdAt DESC in GetOrderByToken

--- repository/order.go
+++ repository/order.go
@@ -8,6 +8,7 @@ import (
 	"github.com/pkg/errors"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type OrderRepository struct {
@@ -96,7 +97,9 @@ func (or *OrderRepository) GetOrderByToken(token string) ([]model.Order, error) 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	cur, err := or.col.Find(ctx, bson.D{{"orderBy", token}})
+	opts := options.Find().SetSort(bson.D{{"createdAt", -1}})
+
+	cur, err := or.col.Find(ctx, bson.D{{"orderBy", token}}, opts)
 	if err != nil {
 		return []model.Order{}, err
 	}
