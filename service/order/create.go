f983113d5c3cece695935efdbcfb51ba117bf68b
melon <vitaya.p@ku.th>
2024-02-01 12:57:38 +0700

    Fix checking wrong menu limit on create order

--- service/order/create.go
+++ service/order/create.go
@@ -23,11 +23,10 @@ func (os *OrderService) Create(req CreateOrderRequest, c *model.Client) error {
 		// validate menuItemId
 		errBit := 1
 		for _, promotionMenuItem := range p.PromotionMenuItems {
-			if roi.Quantity > promotionMenuItem.Limit {
-				return InvalidOrderQuantity
-			}
-
 			if promotionMenuItem.MenuItemId == roi.MenuItemId {
+				if roi.Quantity > promotionMenuItem.Limit {
+					return InvalidOrderQuantity
+				}
 				errBit = 0
 				break
 			}
